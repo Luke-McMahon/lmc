c061b88f76f6218e237e74e74dbb10f5e7966f6b
Luke McMahon <luke.mcmahon@service.vic.gov.au>
2023-06-21 15:32:52 +1000

    feat: pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Luke-McMahon.github.io',
   plugins: [vue()],
-})
+});
